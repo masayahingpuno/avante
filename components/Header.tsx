77f3ba634d48a4b2a062091754b5774e94230bc0
masayahingpuno <79151036+masayahingpuno@users.noreply.github.com>
2022-02-21 01:15:03 +0800

    Update the logo size

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,22 +6,22 @@ function Header() {
   return (
     <div>
       <div className="container mx-auto flex h-14 w-screen justify-between bg-slate-100 pt-8">
-          <Link href="/" >
-            <a className="flex items-center justify-end md:w-1/6 md:justify-start">
-              <div className="p-2 pl-6 md:p-2">
-            <Image
-              src="/logo4.png"
-              alt="logo"
-              quality={100}
-              width={35}
-              height={35}
+        <Link href="/">
+          <a className="flex items-center justify-end md:w-1/4 md:justify-start">
+            <div className="p-2 pl-6 md:p-2">
+              <Image
+                src="/logo4.png"
+                alt="logo"
+                quality={100}
+                width={35}
+                height={35}
               />
-              </div>
-              <div className="font-main text-2xl font-semibold	 text-black subpixel-antialiased">
-                agham ai
-              </div>
-            </a>
-          </Link>
+            </div>
+            <div className="font-main text-2xl	font-semibold text-black subpixel-antialiased">
+              agham ai
+            </div>
+          </a>
+        </Link>
         <ul className="hidden w-full items-center justify-end gap-6 font-main font-semibold text-black subpixel-antialiased md:flex">
           <li className="">
             <Link href="/">
